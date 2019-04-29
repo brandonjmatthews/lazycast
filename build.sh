9238a3d1cedc164d74eaf08d883ef160087d9893
Brandon Matthews <brandonjmatthews@outlook.com>
2019-04-29 18:27:20 +0930

    Split apt-get installation and build scripts

--- build.sh
+++ build.sh
@@ -1,5 +1,3 @@
-sudo apt install net-tools python udhcpd
-sudo apt install libx11-dev libasound2-dev libavformat-dev libavcodec-dev
 cd /opt/vc/src/hello_pi/hello_video
 make
 cd /opt/vc/src/hello_pi/libs/ilclient/
---
+++ install.sh
@@ -0,0 +1,2 @@
+sudo apt install net-tools python udhcpd
+sudo apt install libx11-dev libasound2-dev libavformat-dev libavcodec-dev
