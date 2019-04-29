dee5aa9905344a6e2e2e5098688959d8f92a65ba
Brandon Matthews <brandonjmatthews@outlook.com>
2019-04-29 18:26:16 +0930

    Added setup and build scripts

---
+++ build.sh
@@ -0,0 +1,6 @@
+sudo apt install net-tools python udhcpd
+sudo apt install libx11-dev libasound2-dev libavformat-dev libavcodec-dev
+cd /opt/vc/src/hello_pi/hello_video
+make
+cd /opt/vc/src/hello_pi/libs/ilclient/
+make
---
+++ setup.sh
@@ -0,0 +1,9 @@
+chmod +x all.sh
+chmod +x d2.py
+cd player
+chmod +x player.bin
+cd ../h264
+chmod +x h264.bin
+cd ../control
+chmod +x control.bin
+chmod +x controlhidc.bin
